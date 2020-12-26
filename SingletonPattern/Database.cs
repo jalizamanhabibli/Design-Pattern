8d108fdab109dccabdc6dd33c4978366f54b3af7
jalizamanhabibli <elizaman196@gmail.com>
2020-12-27 00:05:31 +0400

    Singleton Pattern

---
+++ SingletonPattern/Database.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace SingletonPattern
+{
+    public class Database
+    {
+        private static Database _instance;
+        private static readonly  object Lock = new object();
+        private readonly string _connectionString;
+
+        private Database(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public static Database GetInstance(string connectionString)
+        {
+            if (_instance == null)
+            {
+                lock (Lock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new Database(connectionString);
+                    }
+                }
+                
+            }
+
+            return _instance;
+        }
+
+        public string ConnectionString => _connectionString;
+    }
+}
---
+++ SingletonPattern/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread thread1 = new Thread(() =>
+            {
+                CreateDatabase("MsSql");
+            });
+
+            Thread thread2 = new Thread(() =>
+            {
+                CreateDatabase("MySql");
+            });
+
+            thread1.Start();
+            thread1.Join();
+
+            thread2.Start();
+            thread2.Join();
+
+            Console.ReadKey();
+        }
+        static void CreateDatabase(string connectionString)
+        {
+            
+            Database database = Database.GetInstance(connectionString);
+            Console.WriteLine(database.ConnectionString);
+        }
+    }
+}
